21f6d85c6aa7e1684c56dcec4db1fbaeea26c3e2
igorelik <gorelik.igor@gmail.com>
2023-08-23 14:13:14 +1000

    Report window placeholder

---
+++ TrainsTake1/ReportView.swift
@@ -0,0 +1,18 @@
+//
+//  ReportView.swift
+//  TrainsTake1
+//
+//  Created by Igor Gorelik on 23/8/2023.
+//
+
+import SwiftUI
+
+struct ReportView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReportView()
+}
